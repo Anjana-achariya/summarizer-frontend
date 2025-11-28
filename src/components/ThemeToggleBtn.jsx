f9a7a65c2d62fd65eb900b5373963284f4c95469
Anjana-achariya <131129025+Anjana-achariya@users.noreply.github.com>
2025-11-28 12:54:26 +0530

    Update ThemeToggleBtn.jsx

--- src/components/ThemeToggleBtn.jsx
+++ src/components/ThemeToggleBtn.jsx
@@ -2,22 +2,20 @@ import React from 'react'
 import toggleDark from "../assets/toggledark.png";
 import toggleLight from "../assets/togglelight.png";
 
-
 const ThemeToggleBtn = ({ theme, setTheme }) => {
   return (
     <button className="p-1.5">
-        {/* rounded-md border border-gray-300  */}
       {theme === "dark" ? (
         <img 
-          src={toggledark} 
+          src={toggleDark} 
           className="w-16 h-16" 
           onClick={() => setTheme("light")}
           alt="darkToggle" 
         />
       ) : (
         <img 
-          src={togglelight} 
-          className="w-16 h-16" 
+          src={toggleLight} 
+          className="w-16 h-16"
           onClick={() => setTheme("dark")}
           alt="lightToggle" 
         />
@@ -26,5 +24,4 @@ const ThemeToggleBtn = ({ theme, setTheme }) => {
   )
 }
 
-export default ThemeToggleBtn
-
+export default ThemeToggleBtn;
